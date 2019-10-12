3497172237ed2563a99213bc27c41a9fc81aadba
saks1209am <30996074+saks1209am@users.noreply.github.com>
2019-10-13 02:15:59 +0530

    Add files via upload

---
+++ For_Loop/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num[]={1,2,5,7,8};
+    for (int i=0; i<=4;i++){
+        cout << num[i] << endl;
+
+    }
+    return 0;
+}
---
+++ Functions/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHi(string name,int age){
+    cout << "Hello " << name << "you are " << age;
+
+}
+
+int main()
+{
+
+    sayHi("Mike",60);
+
+    return 0;
+}
