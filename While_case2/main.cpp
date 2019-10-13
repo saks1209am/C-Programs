e8a11481d8e54338bfc84b921aa233905d1aa9fc
saks1209am <30996074+saks1209am@users.noreply.github.com>
2019-10-14 00:56:00 +0530

    Add files via upload

---
+++ While_case2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int secretNum =7;
+    int guess;
+
+    while(secretNum!=guess){
+        cout << "Enter guess:";
+        cin >> guess;
+
+    }
+
+    cout << "You Win:";
+
+    return 0;
+}
